30e7046108e6fcdb5697e55d4e1a423b73153017
TheBizzle <jason.bertsche@gmail.com>
2012-07-09 10:56:32 -0500

    Enabling the server to accept NetLogo commands over HTTP POST

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.WebWorkspace
+import org.nlogo.headless.HeadlessWorkspace
 
 object Application extends Controller {
-  
+
+  private val modelsURL = "http://localhost:9001/assets/models/"
+  private val modelName = "Wolf Sheep Predation"
+  private val ws = workspace(modelsURL + java.net.URLEncoder.encode(modelName, "UTF-8") + ".nlogo")
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def netlogoCommand = Action {
+    implicit request =>
+      val bod = request.body.asFormUrlEncoded
+      bod map (paramMap => Ok(ws.execute(paramMap("agentType").head, paramMap("cmd").head))) getOrElse (NotAcceptable)
+  }
+
+  private def workspace(url: String) : WebWorkspace = {
+    val wspace = HeadlessWorkspace.newInstance(classOf[WebWorkspace]).asInstanceOf[WebWorkspace]
+    wspace.openString(io.Source.fromURL(url).mkString)
+    wspace
   }
   
 }
--- app/models/WebWorkspace.scala
+++ app/models/WebWorkspace.scala
@@ -5,7 +5,6 @@ import org.nlogo.headless.HeadlessWorkspace
 import org.nlogo.api.{RendererInterface, AggregateManagerInterface}
 import org.nlogo.agent.World
 import org.nlogo.workspace.AbstractWorkspace
-import play.api.libs.json.{JsValue, Json}
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,51 +17,37 @@ class WebWorkspace(world: World, compiler: CompilerInterface, renderer: Renderer
                    aggregateManager: AggregateManagerInterface, hbmFactory: AbstractWorkspace.HubNetManagerFactory)
                    extends HeadlessWorkspace(world, compiler, renderer, aggregateManager, hbmFactory) {
 
-  private var errorOpt: Option[String] = None // Ehh... could be worse
+  // Have to do some state juggling, due to how the `outputAreaBuffer`'s contents are managed...
+  def execute(agentType: String, cmd: String) : String = {
+    outputAreaBuffer.clear()
+    processCommand(agentType, cmd) match { case (input, cmdStr) => generateOutput(runCommand(input, cmdStr)) }
+  }
 
-  def processInput(commandStr: String) {
-    // Tries to parse `commandStr` as a JS object with two properties: `agentType` and `cmd`
-    // (Which debatably sucks, since the JS object's property names could theoretically
-    //  change at any time, effectively breaking this code.)
-    // If it fails to parse as JSON, `commandStr` is treated as a raw NetLogo command string
-    val (input, cmdStr) = {
-      try {
+  private def processCommand(agentType: String, cmd: String) : (String, String) = {
+    val in = "%s> %s\n".format(agentType, cmd)
+    if (agentType != "observer")
+      (in, "ask " + agentType + " [ " + cmd + "\n]")
+    else
+      (in, cmd)
+  }
 
-        def morph(js: JsValue) = js.asOpt[String].get
-
-        val js = Json.parse(commandStr)
-        val (agentType, cmd) = (morph(js \ "agentType"), morph(js \ "cmd"))
-        val in = "%s> %s\n".format(agentType, cmd)
-
-        if (agentType != "observer")
-          (in, "ask " + agentType + " [ " + cmd + "\n]")
-        else
-          (in, cmd)
-
-      }
-      catch {
-        case ex: com.codahale.jerkson.ParsingException => ("", commandStr)
-      }
+  private def runCommand(input: String, cmdStr: String) : Either[String, String] = {
+    try {
+      command(cmdStr)
+      Right(input)
     }
+    catch {
+      case ex: org.nlogo.api.CompilerException => Left("ERROR: " + ex.getLocalizedMessage)
+    }
+  }
 
-    {
-      try {
-        command(cmdStr)
-        Right(input)
-      }
-      catch {
-        case ex: org.nlogo.api.CompilerException => Left("ERROR: " + ex.getLocalizedMessage)
-      }
-    } fold((x => errorOpt = Option(x)), { x => outputAreaBuffer.append(x); errorOpt = None })
-
+  private def generateOutput(resultEither: Either[String, String]) : String = {
+    val errorStr = resultEither fold (("\n" + _), { x => outputAreaBuffer.append(x); "" })
+    outputAreaBuffer.mkString.trim + errorStr
   }
 
   override def sendOutput(oo: org.nlogo.agent.OutputObject, toOutputArea: Boolean) {
     super.sendOutput(oo, true) // This must always be `true` in order for it to show up in the web frontend
-  }
-
-  def getOutput: String = {
-    outputAreaBuffer.mkString.trim + (errorOpt map ("\n" + _) getOrElse(""))
   }
 
 }
