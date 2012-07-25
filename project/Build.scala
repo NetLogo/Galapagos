c4de44dbba6a28636c5f3c06d43abfe1c6b7d92e
TheBizzle <jason.bertsche@gmail.com>
2012-07-25 16:22:01 -0500

    Whoops (correcting grievous mistake from previous commit)

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      libraryDependencies += "asm" % "asm-all" % "3.3.1"
-      libraryDependencies += "log4j" % "log4j" % "1.2.16"
-      libraryDependencies += "org.picocontainer" % "picocontainer" % "2.13.6"
-      libraryDependencies += "org.nlogo" % "NetLogo" % "5.0.1" from "http://ccl.northwestern.edu/netlogo/5.0.1/NetLogo.jar"
-      libraryDependencies += "com.imaginea" %% "socket.io.play" % "0.0.3-SNAPSHOT"
+      "asm" % "asm-all" % "3.3.1",
+      "log4j" % "log4j" % "1.2.16",
+      "org.picocontainer" % "picocontainer" % "2.13.6",
+      "org.nlogo" % "NetLogo" % "5.0.1" from "http://ccl.northwestern.edu/netlogo/5.0.1/NetLogo.jar",
+      "com.imaginea" %% "socket.io.play" % "0.0.3-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(
