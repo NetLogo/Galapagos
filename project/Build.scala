115a240aeade0e5b302ae913b15ea4f376d8dc87
TheBizzle <jason.bertsche@gmail.com>
2012-10-17 14:19:30 -0500

    Cleaning up the main build file
    I don't even know how ASM and Log4J got themselves attached to this thing...

--- project/Build.scala
+++ project/Build.scala
@@ -4,20 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "HeadlessNL"
+  val appName         = "Teletortoise"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "asm" % "asm-all" % "3.3.1",
-    "log4j" % "log4j" % "1.2.16",
     "org.picocontainer" % "picocontainer" % "2.13.6",
-    "org.nlogo" % "NetLogo" % "5.0.2" from "http://ccl.northwestern.edu/netlogo/5.0.2/NetLogo.jar",
-    "com.imaginea" %% "socket.io.play" % "0.0.3-SNAPSHOT"
+    "org.nlogo" % "NetLogo" % "5.0.2" from "http://ccl.northwestern.edu/netlogo/5.0.2/NetLogo.jar"
   )
 
-  val resolverSettings = Seq(
-    resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-  )
+  val resolverSettings = Seq[Setting[_]]()
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     resolverSettings ++ ObtainResources.settings: _*)
