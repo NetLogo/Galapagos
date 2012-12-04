618086808af181c849fdd2c32399f51586650648
TheBizzle <jason.bertsche@gmail.com>
2012-12-04 14:11:13 -0600

    Ensuring that managed libs are versioned

--- project/ObtainResources.scala
+++ project/ObtainResources.scala
@@ -8,8 +8,8 @@ import java.net.URL
 
 object ObtainResources {
 
-  val obtainResources   = TaskKey[Unit]("obtain-resources", "download the JS libs and stylesheets")
-  val obtainJSLibs      = TaskKey[Unit]("obtain-js-libs", "download the JS libs (i.e. jQuery)")
+  val obtainResources   = TaskKey[Unit]("obtain-resources",   "download the JS libs and stylesheets")
+  val obtainJSLibs      = TaskKey[Unit]("obtain-js-libs",     "download the JS libs (i.e. jQuery)")
   val obtainStylesheets = TaskKey[Unit]("obtain-stylesheets", "download the stylesheets (i.e. Bootstrap)")
 
   val settings = Seq[Setting[_]](
@@ -17,24 +17,26 @@ object ObtainResources {
     obtainResources <<=  obtainResources.dependsOn(obtainJSLibs, obtainStylesheets),
     compile in Compile <<= (compile in Compile).dependsOn(obtainResources),
     obtainJSLibs <<= (baseDirectory, streams) map { (base, s) =>
-      obtain(base, s.log.info(_),
-             "javascripts", ".js", Seq("underscore", "jquery", "mousetrap"))},
+      obtain(base, s.log.info(_), "javascripts", ".js", Seq("underscore-1.4.2", "jquery-1.8.3", "mousetrap-1.1.1"))
+    },
     obtainStylesheets <<= (baseDirectory, streams) map { (base, s) =>
-      obtain(base, s.log.info(_),
-             "stylesheets", ".css", Seq("bootstrap"))}
+      obtain(base, s.log.info(_), "stylesheets", ".css", Seq("bootstrap-1.4.0"))
+    }
   )
 
   // does the real work
   private def obtain(base: File, log: String => Unit, folderName: String, fileExt: String, libNames: Seq[String]) {
-    val dir           = base / "public" / folderName / "managed"
-    val namePathPairs = libNames map (_ + fileExt) map (name => (name, dir / name))
+    val dir = base / "public" / folderName / "managed"
     IO.createDirectory(dir)
-    namePathPairs foreach {
-      case (name, path) =>
-        val url = "http://ccl.northwestern.edu/devel/" + name
-        if(!path.exists) {
-          log("downloading " + path)
-          IO.download(new URL(url), path)
+    libNames map {
+      _ + fileExt
+    } map {
+      name => ("http://ccl.northwestern.edu/devel/" + name, dir / name)
+    } foreach {
+      case (url, localPath) =>
+        if(!localPath.exists) {
+          log("downloading " + localPath)
+          IO.download(new URL(url), localPath)
         }
     }
   }
