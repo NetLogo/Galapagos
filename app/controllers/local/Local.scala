3b7b47adb0bba787ef67a1cfe6ec6e13ec97468f
TheBizzle <jason.bertsche@gmail.com>
2013-01-25 10:38:59 -0600

    Moving the 'Local'/'Remote' controllers into their own packages

--- app/controllers/local/Local.scala
+++ app/controllers/local/Local.scala
@@ -1,4 +1,4 @@
-package controllers
+package controllers.local
 
 import
   play.api.{ libs, mvc },
--- app/controllers/remote/Remote.scala
+++ app/controllers/remote/Remote.scala
@@ -1,4 +1,4 @@
-package controllers
+package controllers.remote
 
 import
   play.api.{ libs, mvc },
