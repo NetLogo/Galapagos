55d0d9e14f5930dd94925a088501598285ca6afb
Seth Tisue <seth@tisue.net>
2013-01-28 16:01:04 -0600

    Tortoise: keep the code for both Life and Termites around
    Termites is still the one enabled by default, for now

--- app/models/local/NetLogoCompiler.scala
+++ app/models/local/NetLogoCompiler.scala
@@ -31,7 +31,7 @@ case class NetLogoCompiler(program: Program = Program.empty(), procedures: Proce
   //@ Improve later with more-dynamic selection of configs
   def generateModelState : (String, NetLogoCompiler) = {
     val strCompilerOpt = carefullyCompile {
-      val (js, newProgram, newProcedures) = NetLogoModels.compileLife
+      val (js, newProgram, newProcedures) = NetLogoModels.compileTermites
       (js, NetLogoCompiler(newProgram, newProcedures))
     }
     strCompilerOpt getOrElse (("", this))
@@ -48,79 +48,5 @@ case class NetLogoCompiler(program: Program = Program.empty(), procedures: Proce
         None
     }
   }
-
-}
-
-object NetLogoModels {
-
-  def compileLife =
-    Compiler.compileProcedures(
-      """
-        |turtles-own [next steps]
-        |
-        |to setup
-        |  clear-all
-        |  __ask-sorted patches [
-        |    if random 100 < 20
-        |      [ set pcolor yellow ] ]
-        |  crt 50
-        |  __ask-sorted turtles [
-        |    set color white
-        |    setxy random-xcor random-ycor
-        |    set size 3
-        |    set next 1
-        |  ]
-        |end
-        |
-        |to go
-        |  __ask-sorted turtles
-        |    [ ifelse steps > 0
-        |        [ set steps steps - 1 ]
-        |        [ action
-        |          wiggle ]
-        |      fd 1 ]
-        |end
-        |
-        |to wiggle
-        |  rt random 50
-        |  lt random 50
-        |end
-        |
-        |to action
-        |  ifelse next = 1
-        |    [ searchforchip ]
-        |    [ ifelse next = 2
-        |      [ findnewpile ]
-        |      [ ifelse next = 3
-        |        [ putdownchip ]
-        |        [ getaway ] ] ]
-        |end
-        |
-        |to searchforchip
-        |  if pcolor = yellow
-        |    [ set pcolor black
-        |      set color orange
-        |      set steps 20
-        |      set next 2 ]
-        |end
-        |
-        |to findnewpile
-        |  if pcolor = yellow
-        |    [ set next 3 ]
-        |end
-        |
-        |to putdownchip
-        |  if pcolor = black
-        |   [ set pcolor yellow
-        |     set color white
-        |     set steps 20
-        |     set next 4 ]
-        |end
-        |
-        |to getaway
-        |  if pcolor = black
-        |    [ set next 1 ]
-        |end
-        |""".stripMargin, -20, 20, -20, 20)
 
 }
