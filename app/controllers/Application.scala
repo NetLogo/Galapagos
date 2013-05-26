b7c12d56fa19049563649efc7ee21d3de3d5fce0
Bryan Head <bryan.head@gmail.com>
2013-05-26 18:14:57 -0500

    Add action that returns list of available models

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 import
   play.api.mvc.{ Action, Controller }
 
-import java.net.URLDecoder
+import play.api.libs.json.Json
 
 object Application extends Controller {
 
@@ -18,5 +18,19 @@ object Application extends Controller {
   }
 
   def model(modelName: String) =
-    controllers.Assets.at(path="/public/modelslib", URLDecoder.decode(modelName, "UTF-8"))
+    controllers.Assets.at(path="/public/modelslib", java.net.URLDecoder.decode(modelName, "UTF-8"))
+
+  def modelList = Action {
+    implicit request => {
+      def recursiveListFiles(f: java.io.File): Array[java.io.File] = {
+        val myFiles = f.listFiles
+        myFiles ++ myFiles.filter(_.isDirectory).flatMap(recursiveListFiles)
+      }
+      val parentPath = "public/modelslib/"
+      val nlogoFiles = Seq("Sample Models", "Code Examples", "Curricular Models").
+        flatMap(dir => recursiveListFiles(new java.io.File(parentPath, dir))).
+        filter(_.getName.endsWith(".nlogo"))
+      Ok(Json.stringify(Json.toJson(nlogoFiles.map(_.getPath.drop(parentPath.length).dropRight(".nlogo".length)))))
+    }
+  }
 }
