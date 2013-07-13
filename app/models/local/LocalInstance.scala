0b77174da467058aebc6d994169a11e4a5c9dbac
Bryan Head <bryan.head@gmail.com>
2013-07-13 11:25:23 -0400

    Made the server not crash when someone tries to compile or open on tortoise.

--- app/models/local/LocalInstance.scala
+++ app/models/local/LocalInstance.scala
@@ -65,9 +65,12 @@ class LocalInstance extends Actor with WebInstance {
     broadcast(generateMessage(JSKey, NetLogoUsername, RoomContext, js))
   }
 
-  override protected def compile(source: String) = ???
+  override protected def compile(source: String) =
+    play.api.Logger.error("Tortoise doesn't support general compiling yet.")
+    
 
-  override protected def open(nlogoContents: String) = ???
+  override protected def open(nlogoContents: String) =
+    play.api.Logger.error("Tortoise doesn't support general opening yet.")
 
   private def validateConnection = (true, "")
   private def generateModelStateMessage = {
