9d18ae534b9ba7c38f129a10f49e8934c5498858
Seth Tisue <seth@tisue.net>
2013-07-16 21:19:11 -0500

    latest NetLogo no longer uses Picocontainer

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,6 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     "asm" % "asm-all" % "3.3.1", // Necessary evil
-    "org.picocontainer" % "picocontainer" % "2.13.6",
     "org.nlogo" % "NetLogoHeadless" % "5.1.0-SNAPSHOT" from
       "http://ccl.northwestern.edu/devel/NetLogoHeadless-388d1e8.jar"
   )
