ae670de1744f0744a95a587dafa4bb9093fdab50
Bryan Head <bryan.head@gmail.com>
2013-09-30 17:30:42 -0500

    Upgrade to Tortoise 2015570 Adds resize-world

--- project/Build.scala
+++ project/Build.scala
@@ -13,8 +13,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "asm" % "asm-all" % "3.3.1", // Necessary evil
     "org.picocontainer" % "picocontainer" % "2.13.6",
-    "org.nlogo" % "NetLogoHeadless" % "5.1.0-SNAPSHOT-5e60a37" from
-      "http://ccl.northwestern.edu/devel/NetLogoHeadless-5e60a37.jar"
+    "org.nlogo" % "NetLogoHeadless" % "5.1.0-SNAPSHOT-2015570" from
+      "http://ccl.northwestern.edu/devel/NetLogoHeadless-2015570.jar"
   )
 
   val moreSettings = Seq[Setting[_]](
