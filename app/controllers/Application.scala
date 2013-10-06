d6f3347dba06a95028087598a6373c4698ecb2cd
TheBizzle <jason.bertsche@gmail.com>
2013-10-05 22:43:31 -0500

    Removing unnecessary braces

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -28,7 +28,7 @@ object Application extends Controller {
   }
 
   def modelList = Action {
-    implicit request => {
+    implicit request =>
       def recursiveListFiles(f: java.io.File): Array[java.io.File] = {
         val myFiles = f.listFiles
         myFiles ++ myFiles.filter(_.isDirectory).flatMap(recursiveListFiles)
@@ -38,6 +38,5 @@ object Application extends Controller {
         flatMap(dir => recursiveListFiles(new java.io.File(parentPath, dir))).
         filter(_.getName.endsWith(".nlogo"))
       Ok(Json.stringify(Json.toJson(nlogoFiles.map(_.getPath.drop(parentPath.length).dropRight(".nlogo".length)))))
-    }
   }
 }
