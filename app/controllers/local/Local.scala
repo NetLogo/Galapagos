28fe6521ac5e7241087072e8ffa9bde046531812
TheBizzle <jason.bertsche@gmail.com>
2013-10-06 13:35:44 -0500

    Hiding away some ugly input validation

--- app/controllers/local/Local.scala
+++ app/controllers/local/Local.scala
@@ -7,7 +7,8 @@ import
   scala.util.Try
 
 import
-  scalaz.Scalaz.{ ToApplyOpsUnapply, ToValidationV }
+  scalaz.{ Scalaz, ValidationNel },
+    Scalaz.{ ToApplyOpsUnapply, ToValidationV }
 
 import
   play.api.{ libs, mvc },
@@ -54,52 +55,10 @@ object Local extends Controller {
       val jsURLs = generateTortoiseLiteJsUrls()
       val argMap = request.extractArgMap
 
-      val urlMaybe =
-        argMap get "nlogo_url" map (
-          _.successNel[String]
-        ) getOrElse {
-          MissingArgsMessage.failNel[String]
-        } flatMap {
-          nlogoURL =>
-            Try(new URL(nlogoURL)) map {
-              url => ModelSaver(url, jsURLs).successNel[String]
-            } recover {
-              case ex: MalformedURLException => "Invalid 'nlogo_url' supplied (must be valid URL)".failNel[String]
-            } getOrElse {
-              "An unknown error has occurred in processing your 'nlogo_url' value".failNel[String]
-            }
-        }
+      val fromURL        = maybeBuildFromURL       (argMap, jsURLs, MissingArgsMessage)
+      val fromSrcAndDims = maybeBuildFromSrcAndDims(argMap, jsURLs, MissingArgsMessage)
 
-      val sourceMaybe =
-        argMap get "nlogo" map (
-          _.successNel[String]
-        ) getOrElse {
-          MissingArgsMessage.failNel[String]
-        }
-
-      val DimensionsRegex = {
-        val s = "\\s*"
-        val n = "-?\\d+"
-        s"""$s[($n),$s($n),$s($n),$s($n)]$s""".r
-      }
-
-      val dimensionsMaybe =
-        argMap get "dimensions" map (
-          _.successNel[String]
-        ) getOrElse {
-          MissingArgsMessage.failNel[String]
-        } flatMap {
-          case DimensionsRegex(minX, maxX, minY, maxY) =>
-            (minX.toInt, maxX.toInt, minY.toInt, maxY.toInt).successNel[String]
-          case _ =>
-            "Expected dimensions in the following format: [minX, maxX, minY, maxY]".failNel[(Int, Int, Int, Int)]
-        }
-
-      val fromSrcAndDims = (sourceMaybe |@| dimensionsMaybe) {
-        (source, dimensions) => ModelSaver(source, dimensions, jsURLs)
-      }
-
-      (fromSrcAndDims orElse urlMaybe) fold (
+      (fromSrcAndDims orElse fromURL) fold (
         (nel => ExpectationFailed(nel.list.mkString("\n"))),
         (js  => Ok(views.html.standaloneTortoise(js)))
       )
@@ -130,6 +89,55 @@ object Local extends Controller {
       )
 
     (normalURLs ++ assetURLs) map (route => new URL(route.absoluteURL()))
+
+  }
+
+  private def maybeBuildFromURL(argMap: Map[String, String], jsURLs: Seq[URL], errorStr: String): ValidationNel[String, String] =
+    argMap get "nlogo_url" map (
+      _.successNel[String]
+    ) getOrElse {
+      errorStr.failNel[String]
+    } flatMap {
+      nlogoURL =>
+        Try(new URL(nlogoURL)) map {
+          url => ModelSaver(url, jsURLs).successNel[String]
+        } recover {
+          case ex: MalformedURLException => "Invalid 'nlogo_url' supplied (must be valid URL)".failNel[String]
+        } getOrElse {
+          "An unknown error has occurred in processing your 'nlogo_url' value".failNel[String]
+        }
+    }
+
+  private def maybeBuildFromSrcAndDims(argMap: Map[String, String], jsURLs: Seq[URL], errorStr: String): ValidationNel[String, String] = {
+
+      val sourceMaybe =
+        argMap get "nlogo" map (
+          _.successNel[String]
+          ) getOrElse {
+          errorStr.failNel[String]
+        }
+
+      val DimensionsRegex = {
+        val s = "\\s*"
+        val n = "-?\\d+"
+        s"""$s[($n),$s($n),$s($n),$s($n)]$s""".r
+      }
+
+      val dimensionsMaybe =
+        argMap get "dimensions" map (
+          _.successNel[String]
+          ) getOrElse {
+          errorStr.failNel[String]
+        } flatMap {
+          case DimensionsRegex(minX, maxX, minY, maxY) =>
+            (minX.toInt, maxX.toInt, minY.toInt, maxY.toInt).successNel[String]
+          case _ =>
+            "Expected dimensions in the following format: [minX, maxX, minY, maxY]".failNel[(Int, Int, Int, Int)]
+        }
+
+      (sourceMaybe |@| dimensionsMaybe) {
+        (source, dimensions) => ModelSaver(source, dimensions, jsURLs)
+      }
 
   }
 
