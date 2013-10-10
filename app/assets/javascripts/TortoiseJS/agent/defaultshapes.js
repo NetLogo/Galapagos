9cebe6ddcef59b4a1cf2a3f2dd3ea82484000eeb
TheBizzle <jason.bertsche@gmail.com>
2013-10-10 14:51:10 -0500

    Adding wolf and sheep as default shapes

--- app/assets/javascripts/TortoiseJS/agent/defaultshapes.js
+++ app/assets/javascripts/TortoiseJS/agent/defaultshapes.js
@@ -1919,6 +1919,170 @@ defaultShapes = {
       }
     ]
   },
+  "sheep": {
+    "rotate": false,
+    "elements": [
+      {
+        "x": 203,
+        "y": 65,
+        "diam": 88,
+        "type": "circle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "x": 70,
+        "y": 65,
+        "diam": 162,
+        "type": "circle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "x": 150,
+        "y": 105,
+        "diam": 120,
+        "type": "circle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xcors": [
+          218,
+          240,
+          255,
+          278
+        ],
+        "ycors": [
+          120,
+          165,
+          165,
+          120
+        ],
+        "type": "polygon",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
+        "marked": false
+      },
+      {
+        "x": 214,
+        "y": 72,
+        "diam": 67,
+        "type": "circle",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
+        "marked": false
+      },
+      {
+        "xmin": 164,
+        "ymin": 223,
+        "xmax": 179,
+        "ymax": 298,
+        "type": "rectangle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xcors": [
+          45,
+          30,
+          30,
+          15,
+          45
+        ],
+        "ycors": [
+          285,
+          285,
+          240,
+          195,
+          210
+        ],
+        "type": "polygon",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "x": 3,
+        "y": 83,
+        "diam": 150,
+        "type": "circle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xmin": 65,
+        "ymin": 221,
+        "xmax": 80,
+        "ymax": 296,
+        "type": "rectangle",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xcors": [
+          195,
+          210,
+          210,
+          240,
+          195
+        ],
+        "ycors": [
+          285,
+          285,
+          240,
+          210,
+          210
+        ],
+        "type": "polygon",
+        "color": "rgba(255, 255, 255, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xcors": [
+          276,
+          285,
+          302,
+          294
+        ],
+        "ycors": [
+          85,
+          105,
+          99,
+          83
+        ],
+        "type": "polygon",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
+        "marked": false
+      },
+      {
+        "xcors": [
+          219,
+          210,
+          193,
+          201
+        ],
+        "ycors": [
+          85,
+          105,
+          99,
+          83
+        ],
+        "type": "polygon",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
+        "marked": false
+      }
+    ]
+  },
   "square": {
     "rotate": false,
     "elements": [
@@ -2542,6 +2706,191 @@ defaultShapes = {
         "type": "line",
         "color": "rgba(141, 141, 141, 1.0)",
         "filled": false,
+        "marked": true
+      }
+    ]
+  },
+  "wolf": {
+    "rotate": false,
+    "elements": [
+      {
+        "xcors": [
+          253,
+          245,
+          245
+        ],
+        "ycors": [
+          133,
+          131,
+          133
+        ],
+        "type": "polygon",
+        "color": "rgba(0, 0, 0, 1.0)",
+        "filled": true,
+        "marked": false
+      },
+      {
+        "xcors": [
+          2,
+          13,
+          30,
+          38,
+          38,
+          20,
+          20,
+          27,
+          38,
+          40,
+          31,
+          31,
+          60,
+          68,
+          75,
+          66,
+          65,
+          82,
+          84,
+          100,
+          103,
+          77,
+          79,
+          100,
+          98,
+          119,
+          143,
+          160,
+          166,
+          172,
+          173,
+          167,
+          160,
+          154,
+          169,
+          178,
+          186,
+          198,
+          200,
+          217,
+          219,
+          207,
+          195,
+          192,
+          210,
+          227,
+          242,
+          259,
+          284,
+          277,
+          293,
+          299,
+          297,
+          273,
+          270
+        ],
+        "ycors": [
+          194,
+          197,
+          191,
+          193,
+          205,
+          226,
+          257,
+          265,
+          266,
+          260,
+          253,
+          230,
+          206,
+          198,
+          209,
+          228,
+          243,
+          261,
+          268,
+          267,
+          261,
+          239,
+          231,
+          207,
+          196,
+          201,
+          202,
+          195,
+          210,
+          213,
+          238,
+          251,
+          248,
+          265,
+          264,
+          247,
+          240,
+          260,
+          271,
+          271,
+          262,
+          258,
+          230,
+          198,
+          184,
+          164,
+          144,
+          145,
+          151,
+          141,
+          140,
+          134,
+          127,
+          119,
+          105
+        ],
+        "type": "polygon",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
+        "marked": true
+      },
+      {
+        "xcors": [
+          -1,
+          14,
+          36,
+          40,
+          53,
+          82,
+          134,
+          159,
+          188,
+          227,
+          236,
+          238,
+          268,
+          269,
+          281,
+          269,
+          269
+        ],
+        "ycors": [
+          195,
+          180,
+          166,
+          153,
+          140,
+          131,
+          133,
+          126,
+          115,
+          108,
+          102,
+          98,
+          86,
+          92,
+          87,
+          103,
+          113
+        ],
+        "type": "polygon",
+        "color": "rgba(141, 141, 141, 1.0)",
+        "filled": true,
         "marked": true
       }
     ]
