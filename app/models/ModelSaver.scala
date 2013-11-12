1bbe97e9ca16766e0eda6927258aaec84434a245
TheBizzle <jason.bertsche@gmail.com>
2013-11-11 21:38:32 -0600

    include original NetLogo code, colorized, in standalone pages

--- app/models/ModelSaver.scala
+++ app/models/ModelSaver.scala
@@ -7,19 +7,25 @@ import
   local.NetLogoCompiler,
   Util.usingSource
 
+import
+  org.nlogo.api.{ ModelReader, ModelSection }
+
+// (String, String) is (JavaScript, NetLogo)
+
 object ModelSaver {
 
-  def apply(source: String, dimensions: (Int, Int, Int, Int), urls: Seq[URL]): String = {
+  def apply(source: String, dimensions: (Int, Int, Int, Int), urls: Seq[URL]): (String, String) = {
     val netLogoJS = NetLogoCompiler.fromCodeAndDims(source, dimensions)._2
-    buildJavaScript(netLogoJS, urls)
+    (buildJavaScript(netLogoJS, urls), source)
   }
 
-  def apply(nlogo: String, jsURLs: Seq[URL]): String = {
+  def apply(nlogo: String, jsURLs: Seq[URL]): (String, String) = {
     val netLogoJS = NetLogoCompiler.fromNLogoFile(nlogo)._2
-    buildJavaScript(netLogoJS, jsURLs)
+    (buildJavaScript(netLogoJS, jsURLs),
+     ModelReader.parseModel(nlogo)(ModelSection.Code).mkString("\n"))
   }
 
-  def apply(url: URL, jsURLs: Seq[URL]): String = {
+  def apply(url: URL, jsURLs: Seq[URL]): (String, String) = {
     val nlogoContents = usingSource(_.fromURL(url))(_.mkString)
     apply(nlogoContents, jsURLs)
   }
