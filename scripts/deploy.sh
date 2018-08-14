d2431793f69e1003404c477f245529504ffbf29c
LaCuneta <jeremy.baker@northwestern.edu>
2018-08-14 10:42:43 -0500

    Operations: Move and generalize the AWS deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script can be used to deploy Galapagos to a non-standard AWS location
+# AWS credentials will need to be configured for this script to work
+# Usage (from root of the Galapgos repo): ./scripts/deploy.sh s3-target-bucket/target-folder DISTRIBUTION_ID
+# Example: ./scripts/deploy.sh netlogo-web-experiments-content/nettango E2TDYOH5TZH83M
+
+sbt clean scrapePlay
+cp -Rv public/modelslib/ target/play-scrape/assets/modelslib
+cp target/web/web-modules/main/webjars/lib/chosen/chosen-sprite*.png target/play-scrape/assets/lib/chosen/
+aws s3 sync ./target/play-scrape s3://$1 --delete --acl public-read --exclude "*" --include "*.*" --exclude "*.html"
+aws s3 sync ./target/play-scrape s3://$1 --delete --acl public-read --exclude "*" --include "*[!.]*" --include "*.html" --content-type "text/html; charset=utf-8"
+aws cloudfront create-invalidation --distribution-id $2 --paths "/*"
