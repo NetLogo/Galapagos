08c9e8b3e6b6b551c8624770b2d546f360f9eb8b
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2021-06-09 09:27:15 -0500

    Infrastructure: Update the deploy script for recent changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@
 # Example: ./scripts/deploy.sh netlogo-web-experiments-content/nettango E2TDYOH5TZH83M experiments.netlogoweb.org/nettango
 
 sbt "set scrapeAbsoluteURL := Some(\"$3\")" clean scrapePlay
-cp -Rv public/modelslib/ target/play-scrape/assets/modelslib
-cp -Rv public/nt-modelslib/ target/play-scrape/assets/nt-modelslib
-cp target/web/web-modules/main/webjars/lib/chosen/chosen-sprite*.png target/play-scrape/assets/lib/chosen/
+cp -Rv public/modelslib/ target/play-scrape/assets/
+cp -Rv public/nt-modelslib/ target/play-scrape/assets/
+cp node_modules/chosen-js/chosen-sprite*.png target/play-scrape/assets/chosen-js/
 aws s3 sync ./target/play-scrape s3://$1 --delete --acl public-read --exclude "*" --include "*.*" --exclude "*.html"
 aws s3 sync ./target/play-scrape s3://$1 --delete --acl public-read --exclude "*" --include "*[!.]*" --include "*.html" --content-type "text/html; charset=utf-8"
 aws cloudfront create-invalidation --distribution-id $2 --paths "/*"
