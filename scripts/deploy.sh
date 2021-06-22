f2e47804fc430356951a7dfd33abf0f3f6f0d0fa
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2021-06-22 09:56:03 -0500

    Infrastructure: Update deploy script notes with emergency usage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 # This script can be used to deploy Galapagos to a non-standard AWS location
-# AWS credentials will need to be configured for this script to work
-# Usage (from root of the Galapgos repo): ./scripts/deploy.sh s3-target-bucket/target-folder DISTRIBUTION_ID url/path
-# Example: ./scripts/deploy.sh netlogo-web-experiments-content/nettango E2TDYOH5TZH83M experiments.netlogoweb.org/nettango
+# AWS credentials will need to be configured for this script to work.  See the
+# AWS command line tools docs for how to do that.
+
+# Usage (from root of the Galapgos repo):
+# ./scripts/deploy.sh s3-target-bucket/target-folder DISTRIBUTION_ID url/path
+
+# Example deploying to experiments site in subfolder /nettango/:
+# ./scripts/deploy.sh netlogo-web-experiments-content/nettango E2TDYOH5TZH83M experiments.netlogoweb.org/nettango
+
+# For reference, if there is an emergency, these commands can be run to deploy
+# to staging or production in the situation where Jenkins is down or otherwise
+# cannot be used to deploy.
+# ./scripts/deploy.sh netlogo-web-staging-content E360I3EFLPUZR0 staging.netlogoweb.org
+# ./scripts/deploy.sh netlogo-web-prod-content E3AIHWIXSMPCAI netlogoweb.org
 
 sbt "set scrapeAbsoluteURL := Some(\"$3\")" clean scrapePlay
 cp -Rv public/modelslib/ target/play-scrape/assets/
