0e53b81aac3b08afa1f042162ffd856ff589b2ea
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2023-02-13 14:48:06 -0600

    Style: Re-add semicolons to JS files

--- app/assets/javascripts/pages/simulation.js
+++ app/assets/javascripts/pages/simulation.js
@@ -1,134 +1,134 @@
-import { createNotifier, listenerEvents } from "/notifications/listener-events.js"
-import { createDebugListener } from "/notifications/debug-listener.js"
-import { createIframeRelayListener } from "/notifications/iframe-relay-listener.js"
-import { attachQueryHandler } from "/queries/iframe-query-handler.js"
+import { createNotifier, listenerEvents } from "/notifications/listener-events.js";
+import { createDebugListener } from "/notifications/debug-listener.js";
+import { createIframeRelayListener } from "/notifications/iframe-relay-listener.js";
+import { attachQueryHandler } from "/queries/iframe-query-handler.js";
 
-import { fakeStorage, NamespaceStorage } from "/namespace-storage.js"
-import { WipListener } from "/beak/wip-listener.js"
+import { fakeStorage, NamespaceStorage } from "/namespace-storage.js";
+import { WipListener } from "/beak/wip-listener.js";
 
-import "/codemirror-mode.js"
-import AlertDisplay from "/alert-display.js"
-import newModel from "/new-model.js"
-import Tortoise from "/beak/tortoise.js"
+import "/codemirror-mode.js";
+import AlertDisplay from "/alert-display.js";
+import newModel from "/new-model.js";
+import Tortoise from "/beak/tortoise.js";
 
-var loadingOverlay  = document.getElementById('loading-overlay')
-var activeContainer = loadingOverlay
-var modelContainer  = document.querySelector('#netlogo-model-container')
-var nlogoScript     = document.querySelector('#nlogo-code')
+var loadingOverlay  = document.getElementById('loading-overlay');
+var activeContainer = loadingOverlay;
+var modelContainer  = document.querySelector('#netlogo-model-container');
+var nlogoScript     = document.querySelector('#nlogo-code');
 
-const params    = new URLSearchParams(window.location.search)
-const paramKeys = Array.from(params.keys())
+const params    = new URLSearchParams(window.location.search);
+const paramKeys = Array.from(params.keys());
 if (paramKeys.length === 1) {
-  const maybeUrl = paramKeys[0]
+  const maybeUrl = paramKeys[0];
   if (maybeUrl.startsWith('http') && params.get(maybeUrl) === '') {
-    params.delete(maybeUrl)
-    params.set('url', maybeUrl)
-    window.location.search = params.toString()
+    params.delete(maybeUrl);
+    params.set('url', maybeUrl);
+    window.location.search = params.toString();
   }
 }
 
-const listeners = []
+const listeners = [];
 
-const disableWorkInProgress = params.has('disableWorkInProgress')
+const disableWorkInProgress = params.has('disableWorkInProgress');
 const [wipListener, getWorkInProgress] = (() => {
   if (!disableWorkInProgress) {
-    var ls
+    var ls;
     try {
-      ls = window.localStorage
+      ls = window.localStorage;
     } catch (exception) {
-      ls = fakeStorage()
+      ls = fakeStorage();
     }
-    const storage    = new NamespaceStorage('netLogoWebWip', ls)
-    const storageTag = params.has('storageTag') ? params.get('storageTag') : null
+    const storage    = new NamespaceStorage('netLogoWebWip', ls);
+    const storageTag = params.has('storageTag') ? params.get('storageTag') : null;
     // There is a bit of a circular dep as the `wipListener` is one of the `listeners` fed to `SessionLite`, but the
     // `wipListener` needs to `getNlogo()` from the `SessionLite`.  Since the tangle is event-based I'm not too worried
     // about it, but ideally the nlogo info maintainer could be separate from both and passed in to both.  -Jeremy B
     // January 2023
-    const wl = new WipListener(storage, storageTag)
+    const wl = new WipListener(storage, storageTag);
     const gwip = (nlogoSource) => {
-      wl.nlogoSource = nlogoSource
-      const wipInfo = wl.getWip()
+      wl.nlogoSource = nlogoSource;
+      const wipInfo = wl.getWip();
       if (wipInfo !== null) {
-        nlogoSource.setModelTitle(wipInfo.title)
-        return wipInfo.nlogo
+        nlogoSource.setModelTitle(wipInfo.title);
+        return wipInfo.nlogo;
       }
-      return nlogoSource.nlogo
+      return nlogoSource.nlogo;
     }
-    listeners.push(wl)
-    return [wl, gwip]
+    listeners.push(wl);
+    return [wl, gwip];
   } else {
-    return [null, null]
+    return [null, null];
   }
-})()
+})();
 
 var pageTitle = function(modelTitle) {
   if (modelTitle != null && modelTitle != '') {
-    return 'NetLogo Web: ' + modelTitle
+    return 'NetLogo Web: ' + modelTitle;
   } else {
-    return 'NetLogo Web'
+    return 'NetLogo Web';
   }
 }
 
-globalThis.session = null
-var speed          = 0.0
-var isVertical     = true
+globalThis.session = null;
+var speed          = 0.0;
+var isVertical     = true;
 
 var openSession = function(s) {
-  globalThis.session = s
+  globalThis.session = s;
   if (!disableWorkInProgress) {
-    wipListener.setSession(globalThis.session)
+    wipListener.setSession(globalThis.session);
   }
-  globalThis.session.widgetController.ractive.set('speed', speed)
-  globalThis.session.widgetController.ractive.set('isVertical', isVertical)
-  document.title = pageTitle(globalThis.session.modelTitle())
-  activeContainer = modelContainer
-  globalThis.session.startLoop()
-  alerter.setWidgetController(globalThis.session.widgetController)
+  globalThis.session.widgetController.ractive.set('speed', speed);
+  globalThis.session.widgetController.ractive.set('isVertical', isVertical);
+  document.title = pageTitle(globalThis.session.modelTitle());
+  activeContainer = modelContainer;
+  globalThis.session.startLoop();
+  alerter.setWidgetController(globalThis.session.widgetController);
 }
 
-const isStandaloneHTML = (nlogoScript.textContent.length > 0)
-const isInFrame        = parent !== window
-const alerter          = new AlertDisplay(document.getElementById('alert-container'), isStandaloneHTML)
-const alertDialog      = document.getElementById('alert-dialog')
-listeners.push(alerter)
+const isStandaloneHTML = (nlogoScript.textContent.length > 0);
+const isInFrame        = parent !== window;
+const alerter          = new AlertDisplay(document.getElementById('alert-container'), isStandaloneHTML);
+const alertDialog      = document.getElementById('alert-dialog');
+listeners.push(alerter);
 
 function handleCompileResult(result) {
   if (result.type === 'success') {
-    openSession(result.session)
+    openSession(result.session);
   } else {
     if (result.source === 'compile-recoverable') {
-      openSession(result.session)
+      openSession(result.session);
     } else {
-      activeContainer = alertDialog
-      loadingOverlay.style.display = 'none'
+      activeContainer = alertDialog;
+      loadingOverlay.style.display = 'none';
     }
-    notifyListeners('compiler-error', result.source, result.errors)
+    notifyListeners('compiler-error', result.source, result.errors);
   }
 }
 
 if (params.has('debugEvents')) {
-  const debugListener = createDebugListener(listenerEvents)
-  listeners.push(debugListener)
+  const debugListener = createDebugListener(listenerEvents);
+  listeners.push(debugListener);
 }
 if (isInFrame && params.has('relayIframeEvents')) {
-  const relayListener = createIframeRelayListener(listenerEvents, params.get('relayIframeEvents'))
-  listeners.push(relayListener)
+  const relayListener = createIframeRelayListener(listenerEvents, params.get('relayIframeEvents'));
+  listeners.push(relayListener);
 }
 
-const notifyListeners = createNotifier(listenerEvents, listeners)
+const notifyListeners = createNotifier(listenerEvents, listeners);
 
 if (isInFrame) {
-  const getSession = () => { return globalThis.session }
-  attachQueryHandler(getSession)
+  const getSession = () => { return globalThis.session };
+  attachQueryHandler(getSession);
 }
 
 var loadModel = function(nlogo, sourceType, path, isUndoReversion) {
-  alerter.hide()
-  if (globalThis.session) {
-    globalThis.session.teardown()
+  alerter.hide();
+  if (globalThis.session) {;
+    globalThis.session.teardown();
   }
-  activeContainer = loadingOverlay
-  const nlogoSource = Tortoise.createSource(sourceType, path, nlogo)
+  activeContainer = loadingOverlay;
+  const nlogoSource = Tortoise.createSource(sourceType, path, nlogo);
   Tortoise.fromNlogo(
     nlogoSource
   , modelContainer
@@ -137,75 +137,75 @@ var loadModel = function(nlogo, sourceType, path, isUndoReversion) {
   , handleCompileResult
   , []
   , listeners
-  )
+  );
 }
 
 const parseFloatOrElse = function(str, def) {
-  const f = Number.parseFloat(str)
-  return (f !== NaN ? f : def)
+  const f = Number.parseFloat(str);
+  return (f !== NaN ? f : def);
 }
 
 const clamp = function(min, max, val) {
-  return Math.max(min, Math.min(max, val))
+  return Math.max(min, Math.min(max, val));
 }
 
 const readSpeed = function(params) {
-  return params.has('speed') ? clamp(-1, 1, parseFloatOrElse(params.get('speed'), 0.0)) : 0.0
+  return params.has('speed') ? clamp(-1, 1, parseFloatOrElse(params.get('speed'), 0.0)) : 0.0;
 }
 
 const redirectOnProtocolMismatch = function(url) {
-  const uri = new URL(url)
+  const uri = new URL(url);
   if ('https:' === uri.protocol || 'http:' === window.location.protocol) {
     // we only care if the model is HTTP and the page is HTTPS. -Jeremy B May 2021
-    return true
+    return true;
   }
 
-  const loc         = window.location
-  const isSameHost  = uri.hostname === loc.hostname
-  const isCCL       = uri.hostname === 'ccl.northwestern.edu'
-  const port        = isSameHost && window.debugMode ? '9443' : '443'
-  const newModelUrl = `https://${uri.hostname}:${port}${uri.pathname}`
+  const loc         = window.location;
+  const isSameHost  = uri.hostname === loc.hostname;
+  const isCCL       = uri.hostname === 'ccl.northwestern.edu';
+  const port        = isSameHost && window.debugMode ? '9443' : '443';
+  const newModelUrl = `https://${uri.hostname}:${port}${uri.pathname}`;
 
   // if we're in an iframe we can't even reliably make a link to use
   // so just alert the user.
   if (!isSameHost && !isCCL && isInFrame) {
-    alerter.reportProtocolError(uri, newModelUrl)
-    activeContainer = alertDialog
-    loadingOverlay.style.display = 'none'
-    return false
+    alerter.reportProtocolError(uri, newModelUrl);
+    activeContainer = alertDialog;
+    loadingOverlay.style.display = 'none';
+    return false;
   }
 
-  var newSearch = ''
+  var newSearch = '';
   if (params.has('url')) {
-    params.set('url', newModelUrl)
-    newSearch = params.toString()
+    params.set('url', newModelUrl);
+    newSearch = params.toString();
   } else {
-    newSearch = newModelUrl
+    newSearch = newModelUrl;
   }
 
-  const newHref = `https://${loc.host}${loc.pathname}?${newSearch}`
+  const newHref = `https://${loc.host}${loc.pathname}?${newSearch}`;
 
   // if we're not on the same host the link might work, but let
   // the user know and let them click it.
   if (!isSameHost && !isCCL) {
-    alerter.reportProtocolError(uri, newModelUrl, newHref)
-    activeContainer = alertDialog
-    loadingOverlay.style.display = 'none'
-    return false
+    alerter.reportProtocolError(uri, newModelUrl, newHref);
+    activeContainer = alertDialog;
+    loadingOverlay.style.display = 'none';
+    return false;
   }
 
-  window.location.href = newHref
-  return false
+  window.location.href = newHref;
+  return false;
 }
 
-speed        = readSpeed(params)
-isVertical   = !(params.has('tabs') && params.get('tabs') === 'right')
+speed        = readSpeed(params);
+isVertical   = !(params.has('tabs') && params.get('tabs') === 'right');
 
 if (nlogoScript.textContent.length > 0) {
-  const nlogo  = nlogoScript.textContent
-  const path   = nlogoScript.dataset.filename
-  notifyListeners('model-load', 'script-element')
-  const nlogoSource = Tortoise.createSource('script-element', path, nlogo)
+  const nlogo  = nlogoScript.textContent;
+  const path   = nlogoScript.dataset.filename;
+  notifyListeners('model-load', 'script-element');
+  const nlogoSource = Tortoise.createSource('script-element', path, nlogo);
   Tortoise.fromNlogo(
     nlogoSource
   , modelContainer
@@ -214,13 +214,13 @@ if (nlogoScript.textContent.length > 0) {
   , handleCompileResult
   , []
   , listeners
-  )
+  );
 
 } else if (params.has('url')) {
-  const url = params.get('url')
+  const url = params.get('url');
 
   if (redirectOnProtocolMismatch(url)) {
-    notifyListeners('model-load', 'url', url)
+    notifyListeners('model-load', 'url', url);
     Tortoise.fromURL(
       url
     , modelContainer
@@ -228,74 +228,74 @@ if (nlogoScript.textContent.length > 0) {
     , handleCompileResult
     , []
     , listeners
-    )
+    );
   }
 
 } else {
-  notifyListeners('model-load', 'new-model')
-  loadModel(newModel, 'new', 'NewModel', false)
+  notifyListeners('model-load', 'new-model');
+  loadModel(newModel, 'new', 'NewModel', false);
 }
 
 window.addEventListener('message', function (e) {
   switch (e.data.type) {
     case 'nlw-load-model': {
-      notifyListeners('model-load', 'file', e.data.path)
-      loadModel(e.data.nlogo, 'disk', e.data.path, false)
-      break
+      notifyListeners('model-load', 'file', e.data.path);
+      loadModel(e.data.nlogo, 'disk', e.data.path, false);
+      break;
     }
     case 'nlw-open-new': {
-      notifyListeners('model-load', 'new-model')
-      params.delete('url')
-      window.location.search = params.toString()
-      loadModel(newModel, 'new', 'NewModel', false)
-      break
+      notifyListeners('model-load', 'new-model');
+      params.delete('url');
+      window.location.search = params.toString();
+      loadModel(newModel, 'new', 'NewModel', false);
+      break;
     }
     case 'nlw-revert-wip': {
-      notifyListeners('revert-work-in-progress')
-      wipListener.revertWip()
-      const nlogoSource = wipListener.nlogoSource
+      notifyListeners('revert-work-in-progress');
+      wipListener.revertWip();
+      const nlogoSource = wipListener.nlogoSource;
       loadModel(
         nlogoSource.nlogo
       , nlogoSource.type
       , nlogoSource.type === "url" ? encodeURI(nlogoSource.url) : nlogoSource.fileName
       , false
-      )
-      break
+      );
+      break;
     }
     case 'nlw-undo-revert': {
-      notifyListeners('undo-revert')
-      wipListener.undoRevert()
-      const nlogoSource = wipListener.nlogoSource
+      notifyListeners('undo-revert');
+      wipListener.undoRevert();
+      const nlogoSource = wipListener.nlogoSource;
       loadModel(
         nlogoSource.nlogo
       , nlogoSource.type
       , nlogoSource.type === "url" ? encodeURI(nlogoSource.url) : nlogoSource.fileName
       , true
-      )
-      break
+      );
+      break;
     }
     case 'nlw-update-model-state': {
       globalThis.session.widgetController.setCode(e.data.codeTabContents);
-      break
+      break;
     }
     case 'run-baby-behaviorspace': {
       var reaction =
         function(results) {
           e.source.postMessage({ type: 'baby-behaviorspace-results', id: e.data.id, data: results }, '*')
-        }
-        globalThis.session.asyncRunBabyBehaviorSpace(e.data.config, reaction)
-      break
+        };
+        globalThis.session.asyncRunBabyBehaviorSpace(e.data.config, reaction);
+      break;
     }
     case 'nlw-export-model': {
-      var model = session.getNlogo()
-      e.source.postMessage({ type: 'nlw-export-model-results', id: e.data.id, export: model }, '*')
-      break
+      var model = session.getNlogo();
+      e.source.postMessage({ type: 'nlw-export-model-results', id: e.data.id, export: model }, '*');
+      break;
     }
   }
 })
 
 if (isInFrame) {
-  var width = '', height = ''
+  var width = '', height = '';
   window.setInterval(function() {
     if (
       globalThis.session && (
@@ -304,15 +304,15 @@ if (isInFrame) {
         document.title != pageTitle(globalThis.session.modelTitle())
       )
     ) {
-      document.title = pageTitle(globalThis.session.modelTitle())
-      width = activeContainer.offsetWidth
-      height = activeContainer.offsetHeight
+      document.title = pageTitle(globalThis.session.modelTitle());
+      width = activeContainer.offsetWidth;
+      height = activeContainer.offsetHeight;
       parent.postMessage({
         width:  activeContainer.offsetWidth,
         height: activeContainer.offsetHeight,
         title:  document.title,
         type:   'nlw-resize'
-      }, '*')
+      }, '*');
     }
-  }, 200)
+  }, 200);
 }
--- app/assets/javascripts/pages/tortoise.js
+++ app/assets/javascripts/pages/tortoise.js
@@ -1,64 +1,67 @@
-import { listenForQueryResponses, createQueryMaker } from "/queries/debug-query-maker.js"
-import { bindModelChooser, selectModel, selectModelByURL, handPickedModels } from "/models.js"
+import { listenForQueryResponses, createQueryMaker } from "/queries/debug-query-maker.js";
+import { bindModelChooser, selectModel, selectModelByURL, handPickedModels } from "/models.js";
 
-var modelContainer = document.querySelector('#model-container')
-var hostPrefix     = location.protocol + '//' + location.host
-var pathSplits = location.pathname.split("/")
-if(pathSplits.length > 2) {
-  hostPrefix = hostPrefix + "/" + pathSplits[1]
+var modelContainer = document.querySelector('#model-container');
+var hostPrefix     = location.protocol + '//' + location.host;
+var pathSplits = location.pathname.split("/");
+if (pathSplits.length > 2) {
+  hostPrefix = hostPrefix + "/" + pathSplits[1];
 }
 
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(window.location.search);
 
-var modelFileInput = document.querySelector('#model-file-input')
-modelFileInput.addEventListener('click', function (event) { this.value = '' })
+var modelFileInput = document.querySelector('#model-file-input');
+modelFileInput.addEventListener('click', function (event) { this.value = '' });
 modelFileInput.addEventListener('change', function (event) {
-  var reader = new FileReader()
+  var reader = new FileReader();
   reader.onload = function (e) {
-    openNlogo(e.target.result)
-  }
+    openNlogo(e.target.result);
+  };
   if (this.files.length > 0) {
-    reader.readAsText(this.files[0])
+    reader.readAsText(this.files[0]);
   }
-})
+});
 
 function pickModel(url) {
-  var encoded = encodeURI(hostPrefix + '/assets/' + url)
-  window.location.hash = encoded
+  var encoded = encodeURI(hostPrefix + '/assets/' + url);
+  window.location.hash = encoded;
 }
 
 function pickRandom() {
-  var model = handPickedModels[Math.floor(Math.random() * handPickedModels.length)]
-  selectModel(model)
-  pickModel(model + ".nlogo")
+  var model = handPickedModels[Math.floor(Math.random() * handPickedModels.length)];
+  selectModel(model);
+  pickModel(model + ".nlogo");
 }
 
 function openModelFromUrl(url) {
   if (decodeURI(url) === url) {
-    url = encodeURI(url)
+    url = encodeURI(url);
   }
   if (url === "Load") {
-    selectModel("Select a model")
+    selectModel("Select a model");
     if (modelContainer.contentWindow.location == "about:blank") {
-      modelContainer.contentWindow.location.replace(`./web?${params.toString()}`)
-      modelFileInput.value = ""
+      modelContainer.contentWindow.location.replace(`./web?${params.toString()}`);
+      modelFileInput.value = "";
     }
+
   } else if (url === "NewModel") {
-    selectModel("Select a model")
-    params.delete("url")
-    modelContainer.contentWindow.location.replace(`./web?${params.toString()}`)
-    modelFileInput.value = ""
+    selectModel("Select a model");
+    params.delete("url");
+    modelContainer.contentWindow.location.replace(`./web?${params.toString()}`);
+    modelFileInput.value = "";
+
   } else {
-    selectModelByURL(url)
-    params.set("url", url)
-    modelContainer.contentWindow.location.replace(`./web?${params.toString()}`)
-    modelFileInput.value = ""
+    selectModelByURL(url);
+    params.set("url", url);
+    modelContainer.contentWindow.location.replace(`./web?${params.toString()}`);
+    modelFileInput.value = "";
+
   }
 }
 
 window.addEventListener("hashchange", function(e) {
-  var url = window.location.hash.slice(1)
-  openModelFromUrl(url)
+  var url = window.location.hash.slice(1);
+  openModelFromUrl(url);
 })
 
 window.addEventListener("message", function(e) {
@@ -67,30 +70,30 @@ window.addEventListener("message", function(e) {
 
     case "nlw-resize": {
 
-      var isValid = function(x) { return (typeof x !== "undefined" && x !== null) }
+      var isValid = function(x) { return (typeof x !== "undefined" && x !== null) };
 
-      var height = e.data.height
-      var width  = e.data.width
-      var title  = e.data.title
+      var height = e.data.height;
+      var width  = e.data.width;
+      var title  = e.data.title;
 
       // Quack, quack!
       // Who doesn't love duck typing? --Jason B. (11/9/15)
       if ([height, width, title].every(isValid)) {
-        modelContainer.width               = width
+        modelContainer.width               = width;
         // When we reset the model height, we lose any scrolling that was in place,
         // so we "copy" it back to the main document.  -Jeremy B March 2021
-        const modelScrollTop               = modelContainer.contentDocument.body.scrollTop
-        modelContainer.height              = height
-        document.documentElement.scrollTop = document.documentElement.scrollTop + modelScrollTop
-        document.title                     = title
+        const modelScrollTop               = modelContainer.contentDocument.body.scrollTop;
+        modelContainer.height              = height;
+        document.documentElement.scrollTop = document.documentElement.scrollTop + modelScrollTop;
+        document.title                     = title;
       }
 
       break;
     }
 
     case "nlw-set-hash": {
-      window.location.hash = e.data.hash
-      break
+      window.location.hash = e.data.hash;
+      break;
     }
 
   }
@@ -98,32 +101,32 @@ window.addEventListener("message", function(e) {
 })
 
 function openNlogo(nlogoContents) {
-  window.location.hash = "Load"
-  var filePath = document.getElementById("model-file-input").value
+  window.location.hash = "Load";
+  var filePath = document.getElementById("model-file-input").value;
   modelContainer.contentWindow.postMessage({
     nlogo: nlogoContents,
     path: filePath,
     type: "nlw-load-model"
-  }, "*")
+  }, "*");
 }
 
 function initModel() {
   if (window.location.hash) {
-    var hash = window.location.hash.substring(1)
+    var hash = window.location.hash.substring(1);
     if (hash === "NewModel") {
-      modelContainer.contentWindow.location.replace(`./web?${params.toString()}`)
+      modelContainer.contentWindow.location.replace(`./web?${params.toString()}`);
     } else {
-      openModelFromUrl(hash)
+      openModelFromUrl(hash);
     }
   } else {
-    pickRandom()
+    pickRandom();
   }
 }
 
 bindModelChooser(document.getElementById('tortoise-model-list')
-               , initModel, pickModel, window.environmentMode)
+               , initModel, pickModel, window.environmentMode);
 
 if (params.has('debugQueries')) {
-  window.makeQuery = createQueryMaker(modelContainer)
-  listenForQueryResponses()
+  window.makeQuery = createQueryMaker(modelContainer);
+  listenForQueryResponses();
 }
