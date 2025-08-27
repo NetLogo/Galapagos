6d8f260a6e147497bb106396e32fc17b0ba5814f
Omar Ibrahim <omaribrahim@uchicago.edu>
2025-08-27 12:19:50 -0500

    Swap color-picker dependency for submodule

--- public/pages/color-picker/javascripts/common/Util.js
+++
@@ -1,6 +0,0 @@
-const switchMap = (target, map, orElse) => {
-    return map.has(target) ? unsafe(map.get(target)) : orElse(target);
-};
-const unsafe = (x) => x;
-export { switchMap, unsafe };
-//# sourceMappingURL=Util.js.map
